6ab97cef45385c1bb6f8bfffabc9b8f538712e01
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2023-04-19 20:48:58 -0300

    add content to new page and change language to pt-br

---
+++ pages/project.py
@@ -0,0 +1,6 @@
+from dash import html
+
+project = html.Div([
+    html.B("Resumo"),
+    html.P("Este Projeto Integrador propõe ilustrar a análise de dados sobre conjuntos de dados relacionados com DSTs. Para isso, irá utilizar dados públicos em escala, irá usufruir da plataforma Google Colab para auxiliar na criação das visualizações dos resultados e da linguagem de programação Python para criação do dashboard")
+])
