47840b64f761016e7737cd4f898e51d2c741cf6b
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2023-04-19 13:42:07 -0300

    change csvSource prop value

--- assets/csvList.py
+++ assets/csvList.py
@@ -1,17 +1,18 @@
 inherentNewColumnsNames = {
-            "Entity": "Entity",
-            "Code": "Code",
+    "Entity": "Entity",
+    "Code": "Code",
             "Year": "Ano",
             "Deaths - HIV/AIDS - Sex: Both - Age: Under 5 (Number)": "Idade: Menor do que 5 anos",
             "Deaths - HIV/AIDS - Sex: Both - Age: 70+ years (Number)": "Idade: Mais do que 70 anos",
             "Deaths - HIV/AIDS - Sex: Both - Age: 5-14 years (Number)": "Idade: 5 a 14 anos",
             "Deaths - HIV/AIDS - Sex: Both - Age: 15-49 years (Number)": "Idade: 15 a 49 anos",
             "Deaths - HIV/AIDS - Sex: Both - Age: 50-69 years (Number)": "Idade: 50 a 69 anos"
-        }
+}
+inherentCsvSource = "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/data-bases-bkp/selected-datasets/deaths-from-hiv-by-age.csv"
 
 csvList = [
     {
-        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/tb-related-deaths-hiv.csv",
+        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/data-bases-bkp/selected-datasets/tb-related-deaths-hiv.csv",
         "title": "Mortes estimadas relacionadas à tuberculose entre pessoas vivendo com HIV por ano",
         "yColumn": "Mortes estimadas",
         "newColumnsNames": {
@@ -22,31 +23,31 @@ csvList = [
         }
     },
     {
-        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/deaths-from-hiv-by-age.csv",
+        "csvSource": inherentCsvSource,
         "title": "Mortes relacionadas com HIV/AIDS em pessoas com menos de 5 anos",
         "yColumn": "Idade: Menor do que 5 anos",
         "newColumnsNames": inherentNewColumnsNames
     },
     {
-        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/deaths-from-hiv-by-age.csv",
+        "csvSource": inherentCsvSource,
         "title": "Mortes relacionadas com HIV/AIDS em pessoas com mais do que 70 anos por ano",
         "yColumn": "Idade: Mais do que 70 anos",
         "newColumnsNames": inherentNewColumnsNames
     },
     {
-        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/deaths-from-hiv-by-age.csv",
+        "csvSource": inherentCsvSource,
         "title": "Mortes relacionadas com HIV/AIDS em pessoas entre 5 a 14 anos por ano",
         "yColumn": "Idade: 5 a 14 anos",
         "newColumnsNames": inherentNewColumnsNames
     },
     {
-        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/deaths-from-hiv-by-age.csv",
+        "csvSource": inherentCsvSource,
         "title": "Mortes relacionadas com HIV/AIDS em pessoas entre 15 a 49 anos por ano",
         "yColumn": "Idade: 15 a 49 anos",
         "newColumnsNames": inherentNewColumnsNames
     },
     {
-        "csvSource": "https://raw.githubusercontent.com/anabeatrizzz/pi-quatro-univesp/master/deaths-from-hiv-by-age.csv",
+        "csvSource": inherentCsvSource,
         "title": "Mortes relacionadas com HIV/AIDS em pessoas entre 50 a 69 anos por ano",
         "yColumn": "Idade: 50 a 69 anos",
         "newColumnsNames": inherentNewColumnsNames
