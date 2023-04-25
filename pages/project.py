ba795e0a2f37553feb06b41d5e37dd1db374ea18
rsgamboa <rsgamboa@github.com>
2023-04-25 18:57:23 -0300

    #23 --project-css

--- pages/project.py
+++ pages/project.py
@@ -2,5 +2,7 @@ from dash import html
 
 project = html.Div([
     html.B("O Projeto", className="section-title"),
-    html.P("Este Projeto Integrador propõe ilustrar a análise de dados sobre conjuntos de dados relacionados com DSTs. Para isso, irá utilizar dados públicos em escala, irá usufruir da plataforma Google Colab para auxiliar na criação das visualizações dos resultados e da linguagem de programação Python para criação do dashboard")
+    html.P("Este Projeto Integrador propõe ilustrar a análise de dados sobre conjuntos de dados relacionados com DSTs. Para isso, irá utilizar dados públicos em escala, irá usufruir da plataforma Google Colab para auxiliar na criação das visualizações dos resultados e da linguagem de programação Python para criação do dashboard", className="section-subtt"),
 ])
+
+
