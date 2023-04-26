73c8bc4085fe546cb41f024602fbee598afccb36
rsgamboa <rsgamboa@github.com>
2023-04-26 17:36:36 -0300

    #25 --members-css

--- pages/project.py
+++ pages/project.py
@@ -7,7 +7,7 @@ project = html.Div([
                 html.Div([
                     html.Div([
                         html.H4("O Projeto"),
-                        html.P("Este Projeto Integrador propõe ilustrar a análise de dados sobre conjuntos de dados relacionados com DSTs. Para isso, irá utilizar dados públicos em escala, irá usufruir da plataforma Google Colab para auxiliar na criação das visualizações dos resultados e da linguagem de programação Python para criação do dashboard"),     
+                        html.P("Este Projeto Integrador propõe ilustrar a análise de dados sobre conjuntos de dados relacionados com DSTs. Para isso, irá utilizar dados públicos em escala, irá usufruir da plataforma Google Colab para auxiliar na criação das visualizações dos resultados e da linguagem de programação Python para criação do dashboard"),
                     ], className="section-title"),
 
                 ], className="col-xl-8 mx-auto text-center"),
