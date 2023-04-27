d1bfac9c88fc1280e9bd811f55dd32a66fa2b744
rsgamboa <rsgamboa@github.com>
2023-04-27 00:21:06 -0300

    #27 --members-css-finished

--- pages/members.py
+++ pages/members.py
@@ -1,32 +1,29 @@
 from dash import html
 
+members = html.Div([
+    html.Section([    
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.H4("A Equipe"),
+                        html.P("A Dashboard desnvolvido pelos alunos da disciplina de Projeto Integrador IV para o Eixo de Computação da Universidade Virtual do Estado de São Paulo (UNIVESP)."),
+                    ], className="section-title"),
 
-def getMemberParagraph():
-    membersListParagraph = []
-    membersList = [
-        "Abner Veloso Bezerra | E-mail: 1701120@aluno.univesp.br",
-        "Alan Alves da Silva | E-mail: 2002522@aluno.univesp.br",
-        "Ana Beatriz Augusto de Melo da Silva | E-mail: 2004708@aluno.univesp.br",
-        "Clovis Garcia | E-mail: 2006404@aluno.univesp.br",
-        "Isabella De Oliveira Tolentino | E-mail: 2002812@aluno.univesp.br",
-        "Lucas Dos Santos Dias | E-mail: 2000549@aluno.univesp.br",
-        "Rafael Fortes Gatto | E-mail: 2014805@aluno.univesp.br",
-        "Rodrigo Sanches Gamboa | E-mail: 2002899@aluno.univesp.br"
-    ]
+                ], className="col-xl-8 mx-auto text-center"),
 
-    for c in range(len(membersList)):
-        membersListParagraph.append(
-            html.P(membersList[c])
-        )
+          ], className="row"),
 
-    return membersListParagraph
+        html.Button(["Rodrigo Samches Gamboa"], className="link", id="btn"),
+        html.Button(["Ana Beatriz"], className="link", id="btn"),
+        html.Button(["Clovis Garcia"], className="link", id="btn"),
+        html.Button(["Isabella De Oliveira"], className="link", id="btn"),
+        html.Button(["Rafael Fortes"], className="link", id="btn"),
+        html.Button(["Abner Veloso"], className="link", id="btn"),
+        html.Button(["Alan Alves"], className="link", id="btn"),
+        html.Button(["Lucas Dos Santos"], className="link", id="btn"), 
 
-
-members = html.Div(
-    [
-        html.B("A Equipe", className="section-title"),
-        html.Br(),
-        html.Br(),
-        html.Div(getMemberParagraph())
-    ]
-)
+        ], className="container"),
+        
+    ], className="experience pt-100 pb-100", id="experience"),
+])
